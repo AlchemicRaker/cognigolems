132d74fff2562424ea3968dd4af4de47f26fbdde
Zachery Delafosse <natsogames@yahoo.com>
2021-03-23 19:19:16 -0500

    added hooks for saving and loading entity data

--- src/main/java/com/syntheticentropy/cogni/forge/entity/CognigolemEntity.java
+++ src/main/java/com/syntheticentropy/cogni/forge/entity/CognigolemEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.passive.GolemEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.World;
 
 public class CognigolemEntity extends GolemEntity {
@@ -15,5 +16,15 @@ public class CognigolemEntity extends GolemEntity {
 
     public static AttributeModifierMap.MutableAttribute createAttributes() {
         return MobEntity.createMobAttributes().add(Attributes.MAX_HEALTH, 100.0D).add(Attributes.MOVEMENT_SPEED, 0.25D).add(Attributes.KNOCKBACK_RESISTANCE, 1.0D).add(Attributes.ATTACK_DAMAGE, 15.0D);
+    }
+
+    @Override
+    public void load(CompoundNBT compoundNBT) {
+        super.load(compoundNBT);
+    }
+
+    @Override
+    public boolean save(CompoundNBT compoundNBT) {
+        return super.save(compoundNBT);
     }
 }
